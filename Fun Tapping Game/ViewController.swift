a30f84615ae3efd3d521b94e537eb5ffd89443a7
Sharon Devasia <sharondevasia@Sharons-MacBook-Air.local>
2016-05-15 00:53:49 +0530

    added files for tapper game

--- Fun Tapping Game/ViewController.swift
+++ Fun Tapping Game/ViewController.swift
@@ -9,17 +9,122 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //properties
+    var maxTaps:Int = 0
+    var currentTaps:Int = 0
+    
+    
+    
+    
+    
+    // the first view elements that will be visible when the screen loads for the first time
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var logoImg:UIImageView!
+    @IBOutlet weak var howManyTapsTxt:UITextField!
+    @IBOutlet weak var playBtn:UIButton!
+    
+    // the list of elements that will appear once the play button is pressed
+    
+    @IBOutlet weak var tapBtn:UIButton!
+    @IBOutlet weak var tapsLbl:UILabel!
+    
+    // a function to handle the target button pressed 
+    
+    @IBAction func onCoinTapped(sender: UIButton) {
+        currentTaps = currentTaps + 1
+        //tapsLbl.text = "\(currentTaps) Taps"
+        updateTapsLbl()
+        
+        if isGameOver(){
+            restartGame()
+            
+        }
+        
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //defining a function to start the game and hide the unwanted elements on screen
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        
+        //validation if the user enters nothing (empty string) in the how mant taps text field
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            //tapsLbl.text = "\(currentTaps) Taps"
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            updateTapsLbl()
+            
+        }
+        
     }
-
+    
+    //function to restart a game 
+    
+    func restartGame(){
+        
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        currentTaps = 0
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+        
+    }
+    
+    
+    
+    
+    
+    
+    //creating a function to check if the game is over 
+    
+    func isGameOver() -> Bool {
+        
+        
+        if currentTaps >= maxTaps {
+            
+            return true
+        } else {
+            
+            return false
+        }
+    }
+    
+    //we created this function because "tapsLbl.text = "\(currentTaps) Taps" are getting repeated and hence following Dont Repeat Yourself (DRY) principle. The above code has been include and commented out to make you understand how same code swells up the program //
+    
+    func updateTapsLbl(){
+        
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
 
 }
 
